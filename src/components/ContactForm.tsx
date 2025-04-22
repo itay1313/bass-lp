c39a45492ac0a7bc12461cba2adfd84d147a5c67
Itay Haephrati <itay1313@gmail.com>
2025-04-22 09:51:54 +0300

    padding top

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -95,7 +95,7 @@ export default function ContactForm() {
         }}
       />
 
-      <div className="container mx-auto px-4 py-16 max-w-lg relative">
+      <div className="container mx-auto px-4 pt-24 pb-16 max-w-lg relative">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold mb-4" style={{ color: 'var(--text)' }}>
             {translations.title}
